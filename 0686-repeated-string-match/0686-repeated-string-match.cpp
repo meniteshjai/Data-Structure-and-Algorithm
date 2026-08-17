2b12c33b108904d94284e4569ade30bb60883845
meniteshjai <nkumar04621@gmail.com>
2026-08-17 19:47:56 +0530

    Time: 61 ms (11.45%), Space: 154.2 MB (6.37%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        int count=1;
+        string s=a;
+        while(s.find(b) ==string::npos){
+            count++;
+            s=s+a;
+            if(s.find(b) !=string::npos){
+                return count;
+            }
+            if(s.length()>b.length()+a.length()){
+               return -1;
+            }
+        }
+        return count;
+    }
+};
