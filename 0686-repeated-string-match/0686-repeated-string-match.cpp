72dfbcce6c0251acb5c8202becac1ac38cc36870
meniteshjai <nkumar04621@gmail.com>
2026-08-17 19:58:30 +0530

    Time: 10 ms (18.43%), Space: 9.7 MB (65.62%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        int count=1;
-        string s=a;
-        while(s.find(b) ==string::npos){
+            // optimize code
+            string ans="";
+            int count=0;
+        while(ans.length()<b.length()){
+            ans+=a;
             count++;
-            s=s+a;
-            if(s.find(b) !=string::npos){
-                return count;
-            }
-            if(s.length()>b.length()+a.length()){
-               return -1;
-            }
         }
-        return count;
+        if(ans.find(b) !=string::npos){
+            return count;
+        }
+        ans+=a;
+        count++;
+
+        if(ans.find(b) !=string::npos){
+            return count;
+        }
+        return -1;
+            // brute force.
+
+        // int count=1;
+        // string s=a;
+        // while(s.find(b) ==string::npos){
+        //     count++;
+        //     s=s+a;
+        //     if(s.find(b) !=string::npos){
+        //         return count;
+        //     }
+        //     if(s.length()>b.length()+a.length()){
+        //        return -1;
+        //     }
+        // }
+        // return count;
     }
 };
