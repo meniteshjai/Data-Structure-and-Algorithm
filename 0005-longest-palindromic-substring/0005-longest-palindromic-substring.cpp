31f062472e02989fa252c7089ea21905dc81885d
meniteshjai <nkumar04621@gmail.com>
2026-08-20 14:51:39 +0530

    Time: 15 ms (45.95%), Space: 18.5 MB (36.47%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string check(string& s,int i,int j){
+  
+        while(i>=0 && j<s.length() && s[i]==s[j]){
+            i--;
+            j++;
+        }
+        i++;
+        j--;
+        return s.substr(i,j-i+1);
+    }
+    string longestPalindrome(string s) {
+        string ans="";
+        for(int centre=0;centre<s.size();centre++){
+            string odd=check(s,centre,centre);
+            
+            string even="";
+            if(centre+1<s.size()){
+                even=check(s,centre,centre+1);
+            }
+            if(odd.length()> ans.length()){
+                ans=odd;
+            }
+            if(even.length()>ans.length()){
+                ans=even;
+            }   
+        } 
+        return ans;  
+    }
+};
